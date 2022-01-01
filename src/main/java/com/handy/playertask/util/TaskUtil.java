525b2645605c8c0477474b6466f663270e061b8d
handy <13546606929@163.com>
2022-01-01 17:43:23 +0800

    Changes

---
+++ src/main/java/com/handy/playertask/util/TaskUtil.java
@@ -0,0 +1,25 @@
+package com.handy.playertask.util;
+
+import com.handy.playertask.entity.TaskPlayerDemand;
+
+import java.util.*;
+
+/**
+ * 常用方法
+ *
+ * @author handy
+ **/
+public class TaskUtil {
+
+    /**
+     * 任务目标进度替换map
+     *
+     * @param taskPlayerDemands 任务目标
+     * @return map
+     * @since 1.10.0
+     */
+    public static List<String> taskScheduleReplaceMap(List<TaskPlayerDemand> taskPlayerDemands) {
+        return new ArrayList<>();
+    }
+
+}
